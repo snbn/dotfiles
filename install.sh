d97b1280039b9a7a787a48baf00b7e5aea0e7b36
snbn <sb.as.engineer@example.com>
2024-02-25 18:25:50 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+root_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+declare -A setting=(
+    ["alacritty"]="$HOME/.config/alacritty"
+    ["conky"]="$HOME/.config/conky"
+    ["i3"]="$HOME/.config/i3"
+    ["ncmpcpp"]="$HOME/.config/ncmpcpp"
+    ["polybar"]="$HOME/.config/polybar"
+    ["ranger"]="$HOME/.config/ranger"
+    ["picom.conf"]="$HOME/.config/picom.conf"
+    ["mpd.conf"]="$HOME/.config/mpd/mpd.conf"
+    [".Xresources"]="$HOME/.Xresources"
+    [".bash_profile"]="$HOME/.bash_profile"
+    [".bashrc"]="$HOME/.bashrc"
+)
+
+for k in "${!setting[@]}"; do
+    ln -s -T "$root_dir/$k" "${setting[$k]}"
+done
